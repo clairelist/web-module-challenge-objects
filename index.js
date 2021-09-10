5de46c0cc1ee85a06f80fb8f1e36bb9f6c4902a1
Claire List <claire.list2501@gmail.com>
2021-09-09 18:16:02 -0600

    finished up task 7 on to strech goal

--- index.js
+++ index.js
@@ -128,10 +128,7 @@ Use the getReviewByIndex function below to do the following:
 
  function getReviewByIndex(array, number) {
     return `${array[number].name} gave the restaurant a ${array[number].rating} star review, and their feedback was: ${array[number].feedback}`;
-
-
  }
-
  console.log(getReviewByIndex(reviews,4));
 
   
@@ -148,9 +145,12 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  
+  return `${array[array.length-1].name} gave the restaurant a ${array[array.length-1].rating} star review, and their feedback was: ${array[array.length-1].feedback}`;
 } 
+
+console.log(getLastReview(reviews));
 
 
 
